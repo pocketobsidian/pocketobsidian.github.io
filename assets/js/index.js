b8f6bd541450b157cb0531f61e560b5d9f2eab24
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2022-10-13 12:22:53 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict'
 
-  document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+  document.getElementById('navbarSideCollapse').addEventListener('click', function () {
     document.querySelector('.offcanvas-collapse').classList.toggle('open')
   })
 })();
